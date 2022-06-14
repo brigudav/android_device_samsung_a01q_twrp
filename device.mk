c783dc84f6e18fe48e52539e85878df1bdb982b4
brigudav <brigudav@gmail.com>
2022-06-14 17:58:35 +1000

    fix lib64 & disable flags

--- device.mk
+++ device.mk
@@ -32,7 +32,7 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libandroidicu
 
 PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib/libandroidicu.so
 
 # Additional binaries & libraries needed for recovery
 #TARGET_RECOVERY_DEVICE_MODULES += \
